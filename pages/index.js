cc6abc6d4ce735273cccb592167e127f83aa82d7
viveralia <edgarv10@gmail.com>
2020-05-15 19:54:03 -0500

    modified index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Head from "next/head";
 import { Grid, Typography, Button, makeStyles } from "@material-ui/core";
 import { useFormik } from "formik";
